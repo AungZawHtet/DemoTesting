633b216ccd2637deafae3585c324b572f20ff123
Your Name <you@example.com>
2017-11-24 12:59:41 +0630

    new push

---
+++ Calendar/src/com/solt/jdc/Demo.java
@@ -0,0 +1,20 @@
+package com.solt.jdc;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+
+public class Demo {
+	public static void main(String[] args) {
+		Instant timeStamp = Instant.now();
+		Instant oneHourLater = timeStamp.plus(1, ChronoUnit.HOURS);
+		System.out.println(timeStamp);
+		System.out.println(oneHourLater);
+
+		LocalDateTime ldt = LocalDateTime.ofInstant(timeStamp, ZoneId.systemDefault());
+		System.out.println(ldt);
+
+	}
+
+}
