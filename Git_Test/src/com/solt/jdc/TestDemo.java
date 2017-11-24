496fe9c260f323caf224a3b46572178b8478bd55
Your Name <you@example.com>
2017-11-24 11:58:38 +0630

    AQWFqef

--- Git_Test/src/com/solt/jdc/TestDemo.java
+++ Git_Test/src/com/solt/jdc/TestDemo.java
@@ -1,12 +1,41 @@
 package com.solt.jdc;
 
-import java.util.Scanner;
-
 public class TestDemo {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	private String name;
+	private int age;
+	private String address;
+	private String grade;
 
-		System.out.println("Enter your favourtite number");
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
 	}
 
 }
